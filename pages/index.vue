f41815264e69f1ddcf57ca7e1533591816c375d3
MrBrown6210 <bite6210@gmail.com>
2021-06-15 17:10:39 +0700

    Done

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,62 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">interview-question-1</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="flex h-screen ">
+    <div class="w-[200px] border-1 border-black flex-none">
+      <input @focusout="check" v-model="integer" class="border-[1px] border-black" type="number">
+    </div>
+    <div class="w-48 border-l-2 border-r-2 border-black flex-1 min-w-[100px]">
+      <select class="border-[1px] border-black" name="type" v-model="mode" @change="check">
+        <option value="isPrime">isPrime</option>
+        <option value="isFibonacci">isFibonacci</option>
+      </select>
+    </div>
+    <div class="w-[300px] border-1 border-black flex-none">
+      <div> {{ result }} </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { defineComponent } from '@nuxtjs/composition-api'
 
-export default Vue.extend({})
+export default defineComponent({
+  setup() {
+    const integer = 1
+    const mode = 'isPrime'
+    const result: any = ''
+    return { integer, mode, result }
+  },
+  methods: {
+    calPrime(num: number) {
+      let isPrime = true
+      for (let i = 2; i < this.integer; i++) {
+        if (this.integer % i === 0) isPrime = false
+      }
+      return isPrime
+    },
+    calFibo(num: number) {
+      let left = 0
+      let right = 1
+      while (right < num) {
+        const _right = right
+        right += left
+        left = _right
+      }
+      return right === num
+    },
+    check(e: Event) {
+      if (this.integer <= 0) this.integer = 1
+      this.integer = Math.round(this.integer)
+
+      if (this.mode === 'isPrime') {
+        this.result = this.calPrime(this.integer)
+      } else {
+        this.result = this.calFibo(this.integer)
+      }
+    }
+  }
+})
 </script>
+
 
 <style>
 /* Sample `apply` at-rules with Tailwind CSS
@@ -37,34 +64,4 @@ export default Vue.extend({})
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
